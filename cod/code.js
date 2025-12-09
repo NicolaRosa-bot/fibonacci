3f19381e2015d36661bd6f56bd485355deb908a0
ROSA NICOLA <14530068@itisrossi.vi.it>
2025-12-09 09:24:14 +0100

    funziona

--- cod/code.js
+++ cod/code.js
@@ -1,16 +1,18 @@
-let v=[];
+
 function stampa(){
+    let vet=[];
   
 
 let N=parseInt(document.getElementById("numero").value);
 
-    if(N>=1){ v.push(0);  }
-    if(N>=2){ v.push(1); }  
+    if(N>=1){ vet.push(0);  }
+    if(N>=2){ vet.push(1); }  
 
     for (let i=2; i<N; i++) {
-        fib[i].push(fib[i-1] + fib[i-2]);
+        vet.push(vet[i-1] + vet[i-2]);
     }
+    console.log(vet);
 
-    document.getElementById("risultato").innerHTML = v.join(", ");
+    document.getElementById("risultato").innerHTML = vet.join(", ");
 }
 
